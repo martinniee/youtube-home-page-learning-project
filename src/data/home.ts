3ea42a675dd8d9df8fb02abc789e8c33171ac044
nathaniel <yang47984@gmail.com>
2023-10-28 23:32:09 +0800

    Category pills section

---
+++ src/data/home.ts
@@ -0,0 +1,16 @@
+export const categories = [
+	'ALL',
+	'JavaScript',
+	'TypeScript',
+	'Programming',
+	'Weight lifting',
+	'Bowling',
+	'Hiking',
+	'React',
+	'Next.js',
+	'Functional Programming',
+	'Object Oriented Programming',
+	'Frontend web development',
+	'Backend web development',
+	'Coding'
+];
